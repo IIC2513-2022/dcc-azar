6ed567034ceedfca7dc7e7da0979019852651cbb
pedroherreraj <pedro.herrera@uc.cl>
2022-04-29 20:19:08 -0400

    feat/recurso-asociado

--- src/models/user.js
+++ src/models/user.js
@@ -14,7 +14,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.hasMany(models.waitingRoom);
     }
   }
   user.init({
